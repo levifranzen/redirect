1f81a0531ccab08625227c1d751d5bac1f0103df
Levi Augusto Franzen <35925385+levifranzen@users.noreply.github.com>
2026-03-02 10:40:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const http = require("http");
 
 const PORT = process.env.PORT || 10000;
+const TARGET = process.env.TARGET_URL;
+
+if (!TARGET) {
+  console.error("TARGET_URL não definido!");
+  process.exit(1);
+}
 
 http.createServer((req, res) => {
+  const location = TARGET.replace(/\/$/, "") + req.url;
+
   res.writeHead(301, {
-    Location: "http://dnsgma.live" + req.url
+    Location: location
   });
+
   res.end();
 }).listen(PORT);
